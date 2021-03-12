45816def58432489ed9de453a231fcebe95792fa
higoramorim <1810042@aluno.univesp.br>
2021-03-12 09:36:35 -0300

    first

---
+++ src/component/Form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Form.css';
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <div className="div-form">
+        <label>_Id</label>
+        <label>Quantity</label>
+        <label>price</label>
+        <label>product</label>
+        <label>client</label>
+        <label>active</label>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
